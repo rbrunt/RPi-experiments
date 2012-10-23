3723c95bc9f3dd5c6ff24d430cee33d6ae906a0a
Richard Brunt <r.brunt@gmail.com>
2012-10-23 15:47:13 +0100

    Added a script that automatically sends you a print of your ifconfig when booted

---
+++ sendIP.py
@@ -0,0 +1,35 @@
+###################################################################################
+# To make this run at startup, add "/path/to/script/sendIP.py &" to /etc/rc.local #
+#       Make sure that you change your email address and SMTP details below!      #
+###################################################################################
+import subprocess
+import smtplib
+
+# Get the output of `ifconfig` and store it in a variable:
+ipinfo = subprocess.check_output("ifconfig", shell=True)
+
+# Setup the Message details:
+fromaddr = 'notifications@blue-shift.net'
+# For multiple addresses, add more to the list. For one address, a string will suffice.
+toaddrs  = ['john.smith@example.com']
+
+msg = """From: RaspberryPi <notifications@blue-shift.net>
+To: John Smith <john.smith@example.com>
+Subject: RPi IP information
+
+Here's a print of ifconfig for the RaspberryPi:
+
+""" + ipinfo
+
+# Credentials for your SMTP server:
+username = 'username'
+password = 'password'
+
+# Acutally sending the mail:
+############################
+# This defaults to using google's smtp, change it to yours if it's different
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
